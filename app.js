6fdff1c8956ce1b8c0b866b09491c59646179c9b
Bryant-Wang24 <485969746@qq.com>
2021-09-21 17:26:35 +0800

    后端渲染

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const http = require('http');
 const router= require('./module/router')
 const  url = require('url')
+const ejs = require('ejs')
 http.createServer(function (request, response) {
     router.static(request,response,'static')
     const pathName = url.parse(request.url).pathname
-    console.log('path',pathName)
     if (pathName==='/login'){
-        response.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
-        response.end('执行登陆')
+        let msg = "数据库里面获取的数据"
+        let list = [
+            {title:1},
+            {title:2},
+            {title:3},
+            {title:4},
+            {title:5}
+        ]
+        ejs.renderFile('./views/login.ejs', {msg,list}, (err, data)=>{
+            response.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+            response.end(data)
+        });
     }else if (pathName==='/register'){
         response.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
         response.end('执行注册')
